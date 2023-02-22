0ec3a724f6bcecb95ad0946c0aef3f30427076d5
Graham Thompson <grwthomps@gmail.com>
2023-02-21 18:33:00 -0600

    Complete exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR"
+    } else if (typeof numOne != "number" || typeof numTwo != "number") {
+        return "ERROR"
+    }
 
+    let highNum = 0
+    let lowNum = 0
+    let sum = 0
+
+    if (numOne < numTwo) {
+        lowNum = numOne
+        highNum = numTwo
+    } else if (numOne > numTwo) {
+        lowNum = numTwo
+        highNum = numOne
+    } else {
+        return "ERROR"
+    }
+
+    for (let i = 0; i <= (highNum - lowNum); i++) {
+        sum += lowNum + i 
+    }
+
+    return sum
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  convertedTemp = (temp - 32) * 5 / 9
+  roundedTemp = Math.round(convertedTemp * 10) / 10
+  return roundedTemp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  convertedTemp = temp * (9/5) + 32
+  roundedTemp = Math.round(convertedTemp * 10) / 10
+  return roundedTemp
 };
 
 // Do not edit below this line
